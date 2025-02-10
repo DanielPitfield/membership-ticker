0e67b40d65971c8c78758635e8faaea73617e65a
Daniel Pitfield <danielpitfield1@gmail.com>
2025-02-10 22:37:55 +0000

    Fetch within server component (instead of route handler)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { DataDisplay } from "@/components/DataDisplay";
-import { MembershipData } from "./api/ticker/route";
+
+type MembershipData = { current_total: number };
+const API_URL = new URL("https://pro-worker.reformparty.uk/ticker/count");
 
 export default async function Home() {
   const numMembers = await (async (): Promise<number> => {
     try {
-      const response = await fetch("http://localhost:3000/api/ticker");
-      const data = await response.json();
+      const response = await fetch(API_URL);
 
-      return data.error ? 0 : (data as MembershipData).current_total;
+      if (!response.ok) {
+        return 0;
+      }
+
+      const data: MembershipData = await response.json();
+
+      return data?.current_total ?? 0;
     } catch (err) {
       return 0;
     }
